d94704cf55f62493c2e172f9a77a4233e9c82779
Rav <44288644+ravshamatov@users.noreply.github.com>
2020-05-14 02:49:20 +0300

    style(parser_output.py): add function comments

--- parser_output.py
+++ parser_output.py
@@ -21,20 +21,44 @@ LOGGING_LEVELS = {
     0: logging.INFO,
     1: logging.DEBUG
 }
+LOG_FILENAME = 'parser_output.log'
 
 
 def parse_file(output_filename, template_filename, print_console=False):
+    """ Parse text file using one TextFSM template
+
+    Arguments:
+        output_filename {str} -- text file for parsing
+        template_filename {str} -- TextFSM template filename
+
+    Keyword Arguments:
+        print_console {bool} -- if True - print to console (default: {False})
+
+    Returns:
+        [list of list of str] -- result table
+    """    
     with open(template_filename) as t, open(output_filename, 'r') as output:
         re_table = textfsm.TextFSM(t)
-
+        result_table = re_table.ParseText(output.read())
         if print_console:
             header = re_table.header
-            logger.warning('\n' + tabulate(re_table.ParseText(output.read()), headers=header))
-        else:
-            return re_table.ParseText(output.read())
+            logger.warning('\n' + tabulate(result_table), headers=header)
+        return result_table
 
 
 def parse_file_many_templates(output_filename, template_filenames, print_console=False):
+    """ Parse file using list of TextFSM templates
+
+    Arguments:
+        output_filename {str} -- text for parsing
+        template_filenames {list of str} -- filenames of TextFSM templates
+
+    Keyword Arguments:
+        print_console {bool} -- if True - return tabulated result with header (default: {False})
+
+    Returns:
+        [list of list of str] -- result
+    """    
     result_rows = [[]]
     header = read_template_headers(template_filenames)
 
@@ -43,22 +67,30 @@ def parse_file_many_templates(output_filename, template_filenames, print_console
         logger.debug(f"File '{output_filename}' - template '{template}'")
         rows = parse_file(output_filename, template, False)
         logger.debug(f"Result: {rows}")
+        ''' Concatenating results '''
         try:
             result_rows = extend_results(result_rows, rows)
             if not rows:
-                logger.warning(f"File '{output_filename}' - template '{template}' - matches not found")
+                logger.warning(
+                    f"File '{output_filename}' - template '{template}' - matches not found")
             logger.debug(f"Total file result: {result_rows}")
         except IndexError:
             logger.info(f"File '{output_filename}' - template '{template}' - matches not found")
-    if print_console:
-        return tabulate(result_rows, headers=header)
-    else:
         logger.debug(f"Summary result: {result_rows}")
         logger.warning(f"File '{output_filename}' - done")
-        return result_rows
+    return tabulate(result_rows, headers=header) if print_console else result_rows
 
 
 def extend_results(table1, table2):
+    """ Concatenate result of parsing by different TextFSM templates
+
+    Arguments:
+        table1 {list of list of str} -- output of parsing by TextFSM template #1
+        table2 {list of list of str} -- output of parsing by TextFSM template #2
+
+    Returns:
+        [list of list of str] -- Result
+    """    
     temp_table = []
     if len(table1) == len(table2):
         for line, row in zip(table1, table2):
@@ -75,12 +107,27 @@ def extend_results(table1, table2):
 
 
 def write_csv_row(filename, row):
+    """ Write row to CSV file
+
+    Arguments:
+        filename {str} -- CSV filename
+        row {list of str} -- Row that will be written
+    """    
     with open(filename, mode='a') as f:
         writer = csv.writer(f, quotechar='"', delimiter=CSV_DELIMETER)
         writer.writerow(row)
 
 
 def parse_directory(directory_path, template_filenames, report_name=None):
+    """ Parse each file in directory with list of TextFSM templates
+
+    Arguments:
+        directory_path {str} -- path to logs directory
+        template_filenames {str} -- filenames of TextFSM templates
+
+    Keyword Arguments:
+        report_name {str} -- CSV report filename (default: generated according datetime)
+    """    
     report_name = report_name or datetime.today().strftime('report_%Y-%m-%d_%H-%M.csv')
     # CSV Header
     write_csv_row(report_name, ['FILENAME'] + read_template_headers(template_filenames))
@@ -92,6 +139,14 @@ def parse_directory(directory_path, template_filenames, report_name=None):
 
 
 def read_templates_list(filename):
+    """ Read file with TextFSM templates and return list of filenames.
+
+    Arguments:
+        filename {str} -- path+filename of text file that contains list of TextFSM filenamess
+
+    Returns:
+        [list of str] -- list of absolute paths to TextFSM filenames
+    """
     dir_path = os.path.dirname(os.path.abspath(filename))
     template_filenames = []
     for line in open(filename):
@@ -106,6 +161,14 @@ def read_templates_list(filename):
 
 
 def read_template_headers(template_filenames):
+    """ Read summary header from all TextFSM templates
+
+    Arguments:
+        template_filenames {list of str} -- filenames of TextFSM tempates
+
+    Returns:
+        [list of str] -- summary header ([SYSNAME, VERSION, HARDWARE, etc..])
+    """    
     logger.debug(f'Reading header for templates {template_filenames}')
     template_headers = []
     for filename in template_filenames:
@@ -122,6 +185,11 @@ def read_template_headers(template_filenames):
 
 
 def read_arguments():
+    """ Parse script arguments
+
+    Returns:
+        [namespace] -- all entered arguments
+    """    
     parser = ArgumentParser(description=SCRIPT_DESCRIPTION)
     parser.add_argument('-D', '--directory_path', help='Absolute or relative path to directory', required=False)
     parser.add_argument('-f', '--filename', help='Absolute or relative path to text file', required=False)
@@ -133,6 +201,11 @@ def read_arguments():
 
 
 def configure_logging(logging_level):
+    """ Configure logging (console and to file)
+
+    Arguments:
+        logging_level {int} -- level of logging (10/20/30/40/50)
+    """    
     logger.setLevel(logging_level)
 
     ch = logging.StreamHandler()
@@ -140,7 +213,7 @@ def configure_logging(logging_level):
     ch.setFormatter(
         logging.Formatter('%(message)s'))
 
-    fh = logging.FileHandler('parser_output.log')
+    fh = logging.FileHandler(LOG_FILENAME)
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(
         logging.Formatter('%(asctime)s:%(name)s:%(levelname)s\t%(message)s'))
@@ -153,8 +226,14 @@ def configure_logging(logging_level):
 
 if __name__ == '__main__':
     """ Main function:
-        1. Parse file if command was like ./parser_output.py -t display_elabel.template -f filename.log
-        2. Parse all files in directory if command was ./parser_output.py -t display_elabel.template -D data2
+        1. Parse one file by one template 
+            >>> ./parser_output.py -t display_elabel.template -f filename.log
+        2. Parse all files in directory by one template
+            >>> ./parser_output.py -t display_elabel.template -D data2
+        3. Parse one file by several templates
+            >>> ./parser_output.py -tl file-with-templates-list.txt -f filename.log
+        4. Parse all files in directory by several templates
+            >>> ./parser_output.py -tl file-with-templates-list.txt -D data2
     """
     args = read_arguments()
     # If no -V arg          then file logging level is INFO
