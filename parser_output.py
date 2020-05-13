5e2b70eb6c5e99a409a0cda74355b49191f6d6c2
Rav <44288644+ravshamatov@users.noreply.github.com>
2020-05-13 17:24:50 +0300

    ft(parser_output.py): parsing one file using multiple textfsm templates

--- parser_output.py
+++ parser_output.py
@@ -28,6 +28,18 @@ def parse_file(template_filename, output_filename, print_console=False):
         #result = [[output_filename] + row for row in )]
         #return result
 
+def parse_file_many_templates(output_filename, template_filenames, print_console=False):
+    result_line = []
+    for template in template_filenames:
+        print(f'Parsing with template {template}:')
+        row = parse_file(template, output_filename, print_console)
+        print(row)
+        try: 
+            result_line.extend(row[0])
+        except IndexError:
+            print(f"File '{output_filename}' - template '{template}' - not found")
+
+    return result_line
 
 def parse_directory(template_filename, directory_path, report_name=None):
     report_name = report_name or datetime.today().strftime('report_%Y-%m-%d_%H-%M')
@@ -38,15 +50,28 @@ def parse_directory(template_filename, directory_path, report_name=None):
             ['Filename'] + textfsm.TextFSM(t).header)
         # Parsing every file in directory and write result to CSV report
         for filename in os.listdir(directory_path):
-            for row in parse_file(template_filename, directory_path + '/' + filename):
-                writer.writerow([filename] + row)
+            for row in parse_file(template_filename, os.path.join(directory_path, filename)):
+                writer.writerow([filename].extend(row))
     print(f"Report filename: {report_name}")
+
+def read_templates_list(filename):
+    dir_path = os.path.dirname(os.path.abspath(filename))
+    template_filenames = []
+    for line in open(filename):
+        template_path = os.path.join(dir_path, line.strip())
+        if os.path.exists(template_path):
+            template_filenames.append(template_path)
+            print(f'{template_path}\t- OK')
+        else:    
+            print(f'{template_path}\t- not exists')
+    return template_filenames
 
 def read_arguments():
     parser = ArgumentParser(description=SCRIPT_DESCRIPTION)
     parser.add_argument('-D','--directory_path',help='Absolute or relative path to directory',required=False)
     parser.add_argument('-f','--filename',help='Absolute or relative path to text file',required=False)
-    parser.add_argument('-t','--template',help='Absolute or relative path to TextFSM template',required=True)
+    parser.add_argument('-t','--template',help='Absolute or relative path to TextFSM template',required=False)
+    parser.add_argument('-tl','--templates_list',help='Absolute or relative path to TextFSM templates list',required=False)
     parser.add_argument('-R','--report_name',help='Report name',required=False)
     return parser.parse_args()
 
@@ -56,12 +81,22 @@ if __name__ == '__main__':
         2. Parse all files in directory if command was ./parser_output.py -t display_elabel.template -D data2
     """    
     args = read_arguments()
-    if args.directory_path:
+    if args.directory_path and args.template:
         print(f"Parsing all files from directory '{args.directory_path}'\n")
         parse_directory(args.template, args.directory_path, args.report_name)
-    elif args.filename:
+    elif args.filename and args.template:
         print(f"Parsing file '{args.filename}' using template '{args.template}'\n")
         parse_file(args.template, args.filename, print_console=True)
+    elif args.filename and args.templates_list:
+        #print(f"Reading TextFSM templates list file '{args.templates_list}':\n")
+        
+        print(f"Parsing file '{args.filename}' using templates: '{args.templates_list}'\n")
+        #print('\n'.join(read_templates_list(args.templates_list)), '\n')
+        print(parse_file_many_templates(
+            args.filename, 
+            read_templates_list(args.templates_list),
+            print_console=False))
+
     else:
         print('Please enter path to Directory of File')
     
